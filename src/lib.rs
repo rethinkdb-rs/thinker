18bfc11d0d2db09771a89502788ee25a7c061063
rushmorem <rushmore@webenchanter.com>
2016-05-05 19:51:31 +0200

    Add r2d2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,32 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {
+//! RethinkDB Driver
+//!
+//! ```rust
+//! extern crate thinker;
+//!
+//! use thinker::r;
+//!
+//! # fn main() {
+//! let mut conn = r.connect(Default::default());
+//! # }
+//! ```
+
+extern crate ql2;
+extern crate reql;
+extern crate r2d2;
+extern crate byteorder;
+extern crate bufstream;
+
+pub mod conn;
+
+use conn::{Connection, Opts};
+
+pub struct Reql;
+
+#[allow(non_upper_case_globals)]
+pub const r: Reql = Reql;
+
+impl Reql {
+    pub fn connect(&self, opts: Opts) -> Connection {
+        Connection::new(opts)
     }
 }
