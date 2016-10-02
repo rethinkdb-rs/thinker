595121abf2f3828899830b8217fde546dbb1ebe3
rushmorem <rushmore@webenchanter.com>
2016-10-02 19:21:52 +0200

    Integration test

---
+++ tests/integration.rs
@@ -0,0 +1,25 @@
+extern crate reql;
+extern crate thinker;
+
+use reql::*;
+use thinker::r;
+
+use std::thread;
+
+#[test]
+fn connection_pool_works() {
+    let pool = r.connect(ConnectOpts::default()).unwrap();
+
+    let mut children = vec![];
+
+    for _ in 0..5000 {
+        let pool = pool.clone();
+        children.push(thread::spawn(move || {
+            let _ = pool.get().unwrap();
+        }))
+    }
+
+    for child in children {
+        let _ = child.join();
+    }
+}
